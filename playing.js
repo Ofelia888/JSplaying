9276add7293a1383bc5e87f54800c4d541ba4a0f
Ofelia888 <acbk123@gmail.com>
2024-12-05 11:50:42 +0100

    jkdhekufkr

---
+++ playing.js
@@ -0,0 +1,32 @@
+/*globalThis
+{
+    var nr = document.getElementsById("input").value;
+}*/
+
+function CheckBttnClicked()
+{
+    let nr = document.getElementById("input").value;
+    if (nr > 0 && nr <= 100)
+        {
+        let nrString = nr.toString();
+        let divThree = nr % 3 === 0;
+        let divFive = nrString.endsWith(5) || nrString.endsWith(0);
+
+        if (divThree && divFive)
+            document.getElementById("display").innerHTML = "FizzBuzz!";
+        else
+            document.getElementById("display").innerHTML = nr;
+        }
+    else
+        document.getElementById("display").innerHTML = "Error: out of range";
+}
+
+function SubtractOneBttnClicked()
+{
+    nr--
+}
+
+function AddOneBttnClicked()
+{
+    console.log(nr++);
+}
